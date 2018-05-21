433a22275dc2c9133c9ed4c14268137d5b97b18c
gewashington <gloriaewashington@gmail.com>
2018-05-21 18:19:27 -0400

    Added react-tabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import Home from './components/Home';
+import Landing from './components/Auth/Landing';
 import { Route } from 'react-router-dom';
 import './App.css';
 
+
+//Goal: When button is clicked, component is changed to sign in 
 class App extends Component {
   render() {
     return (
 
       <div className="App">
-        <Route path="/" component={Home} />
+        <Route path="/" component={Landing} />
       </div>
     );
   }
--- src/components/Auth/Landing.js
+++ src/components/Auth/Landing.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { Link, Router, Route, Switch } from 'react-router-dom';
+import { Link, Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import Login from './Login';
 import Signup from './Signup';
+import 'react-tabs/style/react-tabs.css';
+
 
 export default class Landing extends React.Component {
+
 
   render()  {
     return(
       <div>
       <p>Hi from Landing</p>
-      <Switch>
-        <Route exact path="/landing/login" component={Login} />
-        <Route exact path="/landing/signup" component={Signup}/>
-      </Switch>
+      <Tabs>
+    <TabList>
+      <Tab>Login</Tab>
+      <Tab>Sign Up</Tab>
+    </TabList>
+
+    <TabPanel>
+      <Login />
+    </TabPanel>
+    <TabPanel>
+      <Signup />
+    </TabPanel>
+  </Tabs>
       <p>BACKGROUND IMAGE</p>
       </div>
     )
