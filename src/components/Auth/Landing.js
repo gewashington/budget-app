87141d984c1ea6862409d34fee75e6f571ffb04d
gewashington <gloriaewashington@gmail.com>
2018-05-26 12:12:40 -0400

    created new tab bar for landing page

--- src/components/Auth/Landing.js
+++ src/components/Auth/Landing.js
@@ -1,40 +1,41 @@
 import React from 'react';
 import { Link, Router, Route, Switch, Redirect } from 'react-router-dom';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import Login from './Login';
 import Signup from './Signup';
-// import 'react-tabs/style/react-tabs.css';
 import '../CSS/TabsCSS.css';
 import '../Entrance.css';
+import Tabs from '../Tabs/Tabs.js';
 /*TO-DO:
 - Edit CSS
 - Add Auth
 */
 
 export default class Landing extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {active: 'login'};
+  }
 
   render()  {
+
+    const content = {
+      login: <Login />,
+      signup: <Signup />
+    };
+
+
     return(
       <div className="wrapper">
         <div className="main">
         <div className="title-container">
-        <Tabs>
-          <TabList>
-            <Tab>Login</Tab>
-            <Tab>Sign Up</Tab>
-          </TabList>
-          <TabPanel>
-            <div className="form-container">
-            <Login />
-            </div>
-          </TabPanel>
-          <TabPanel>
-            <div className="form-container">
-            <Signup />
-            </div>
-          </TabPanel>
-        </Tabs>
+        <div>
+        <Tabs active={this.state.active}
+          onChange={active => this.setState({active})}>
+          <div key="login">Login</div>
+          <div key="signup">Sign Up</div>
+          </Tabs>
+          <p>{content[this.state.active]}</p>
+        </div>
       </div>
       </div>
       </div>
